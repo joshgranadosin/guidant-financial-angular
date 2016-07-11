c5088f2e260602cd75ce445ae6f9bcaa6c99919a
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-07-11 13:20:51 -0700

    Added angular charts and connected controller

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,5 +1,15 @@
 var controllers = angular.module('GFCtrls', ['GFServices']);
 
 controllers.controller('TableCtrl', ['$scope', function($scope){
-	$scope.test = 'test';
+	$scope.testdata = {
+		user: "Josh Granadosin",
+		data: [
+			{symbol:'GOOGL', type:'stock', shares:100, price:30},
+			{symbol:'MSFT', type:'stock', shares:50, price:10},
+			{symbol:'BNDS', type:'bond', shares:25, price:100},
+			{symbol:'FDFX', type:'funds', shares:10, price:20}
+		]
+	};
+
+
 }])
