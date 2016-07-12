147a6a6d7e8a630a31d0c3c2f12f9ba949c17d7b
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-07-12 14:56:58 -0700

    made the chart responsive

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,6 +1,8 @@
 var controllers = angular.module('GFCtrls', ['GFServices', 'googlechart']);
 
-controllers.controller('TableCtrl', ['$scope', function($scope){
+controllers.controller('TableCtrl', ['$scope', '$window', function($scope, $window){
+
+	// fake data for testing
 	$scope.data = [
 		{symbol:'GOOGL', type:'STOCK', shares:100, price:30},
 		{symbol:'MSFT', type:'STOCK', shares:50, price:10},
@@ -8,17 +10,20 @@ controllers.controller('TableCtrl', ['$scope', function($scope){
 		{symbol:'FDFX', type:'FUND', shares:10, price:20}
 	]
 
+	// required declaration for charts
 	$scope.values = [];
 	$scope.stockVal = 0;
 	$scope.bondVal = 0;
 	$scope.fundVal = 0;
 
+	// calculates each value, declared not called
 	function calcVal(s){
 		if(s.type === 'STOCK'){return s.shares * s.price}
 		else if(s.type === 'BOND'){return s.shares * s.price * 0.8}
 		else if(s.type === 'FUND'){return s.shares * s.price * 0.6}
 	}
 
+	// calculates all values, declared not called
 	function calcAll(){
 		$scope.stockVal = $scope.bondVal = $scope.fundVal = 0;
 		$scope.data.forEach(function(s){
@@ -31,41 +36,75 @@ controllers.controller('TableCtrl', ['$scope', function($scope){
 		});
 	}
 
+	// calls above functions
 	calcAll();
 
-/* pie options */
-	  $scope.myChartObject = {};
-    
-    $scope.myChartObject.type = "PieChart";
+	// declare pie chart
+  $scope.myChartObject = {};
+  $scope.myChartObject.type = "PieChart";
 
-    $scope.myChartObject.data = {
-    	cols: [
-        {id: "t", label: "label", type: "string"},
-        {id: "s", label: "value", type: "number"}
-    	],
-    	rows: [
-        {c: [
-        	{v: "Value of Stocks: " + $scope.stockVal},
-        	{v: $scope.stockVal}
-        	]
-        },
-        {c: [
-        	{v: "Value of Bonds: " + $scope.bondVal},
-        	{v: $scope.bondVal}
-        	]
-        },
-        {c: [
-          {v: "Value of Funds: " + $scope.fundVal},
-          {v: $scope.fundVal},
-        	]
-      	}
-    	]
-  	};
+  // populate data into pie chart
+  $scope.myChartObject.data = {
+  	cols: [
+      {id: "t", label: "label", type: "string"},
+      {id: "s", label: "value", type: "number"}
+  	],
+  	rows: [
+      {c: [
+      	{v: "Stocks"},
+      	{v: $scope.stockVal}
+      	]
+      },
+      {c: [
+      	{v: "Bonds"},
+      	{v: $scope.bondVal}
+      	]
+      },
+      {c: [
+        {v: "Funds"},
+        {v: $scope.fundVal},
+      	]
+    	}
+  	]
+	};
 
-    $scope.myChartObject.options = {
+	// determine if you want to show percentage or total values
+	$scope.pieSliceText = 'percentage';
+	$scope.toggleEnabled = true;
+
+	// determine options based on screen size, declared not called
+	function redrawPieChart(){
+		console.log('redrawing pie');
+		if($window.innerWidth < 650){
+			$scope.myChartObject.options = {
+       title: 'Distribution of Securities',
+        legend: {position:'bottom'},
+        pieSliceText: $scope.pieSliceText,
+        pieStartAngle: 90
+	    };
+	    $scope.toggleEnabled = true;
+		}
+		else{
+			$scope.myChartObject.options = {
         title: 'Distribution of Securities',
-        legend: {position:'labeled'},
-        pieSliceText: 'none'
-    };
+        legend: {position:'labeled', labeledValueText: 'both'},
+        pieSliceText: 'none',
+        pieStartAngle: 90
+	    };
+	    $scope.toggleEnabled = false;
+		}
+	}
+
+	// calls above function, and calls again when screen is resized
+	redrawPieChart();
+	angular.element($window).bind('resize', function(){
+	  redrawPieChart();
+	});
+
+	$scope.togglePie = function(str){
+		$scope.pieSliceText = str;
+		console.log($scope.pieSliceText)
+		redrawPieChart();
+	}
 
 }])
